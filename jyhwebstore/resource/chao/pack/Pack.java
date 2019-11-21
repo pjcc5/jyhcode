1ebaff79ffe5541c0cebb7bb8c36da8f3e794deb
pjcc5 <1783484759@qq.com>
2019-11-22 00:55:51 +0800

    能获取商品照片,价格,图片地址

--- jyhwebstore/resource/chao/pack/Pack.java
+++ jyhwebstore/resource/chao/pack/Pack.java
@@ -20,8 +20,31 @@ public class Pack {
 					Element e1 = element.select("div[class=itemBox]").get(0);
 					//获取商品名称
 					Element e2 = e1.select(".proName").get(0);
-					Element e3 = e2.select(".mainTitle").get(0);
-					System.out.println(e3.attr("title"));
+					if(e2.select(".mainTitle").size()!=0)
+					{
+						Element e3 = e2.select(".mainTitle").get(0);
+						System.out.println(e3.attr("title"));
+						//获取商品价格
+						Element e4 =  e1.select(".proPrice").get(0).select(".num").get(0);
+						String price = e4.attr("yhdprice");
+						System.out.println("价格:"+price);
+						//获取商品图片
+						Elements imgs = e1.select("div[class=proImg]").get(0).select("a[class=img]").get(0).select("img");
+						System.out.println("总共照片数:"+imgs.size());
+						Element img = imgs.get(0);
+						String  imgurl="";
+						if("".equals(img.attr("src")))
+						{	
+							imgurl = img.attr("original").substring(2);
+							
+						}else{
+							imgurl = img.attr("src").substring(2);
+						}
+						System.out.println("地址:"+imgurl);
+						
+					}
+					
+					
 				}
 				 
 				
