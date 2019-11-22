7f3c3368cb780a221ee9001415a08acaf03949df
谭显 <77954@LAPTOP-CO8US9BN>
2019-11-22 17:03:45 +0800

    单态登录

---
+++ jyhwebstore/resource/tan/Singleton.java
@@ -0,0 +1,26 @@
+package tan;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Singleton extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+	
+}
