634b54c6c4c899b13666c7004648b768027bdd9a
maoyange9804 <1329016283@qq.com>
2019-11-23 11:13:27 +0800

    修改db层为数据源结构

--- jyhwebstore/src/db/DbHelp.java
+++ jyhwebstore/src/db/DbHelp.java
@@ -1,46 +1,51 @@
 package db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class DbHelp {
-	private static Properties prop = new Properties();
-	static{
-		try {
-			prop.load(DbHelp.class.getResourceAsStream("/db/prop/db.properties"));
-			Class.forName(prop.getProperty("driver"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("......DB层异常......");
-		}
-	}
-	public static Connection getConnection()
-	{
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
-			System.out.println("......连接成功......");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("......连接异常......");
-		}
-		return conn;
-	}
-	
-	public static void closeConnection(Connection conn)
-	{
-		try {
-			if (conn!=null && !conn.isClosed()) {
-				conn.close();
-				System.out.println("......数据库关闭成功！......");
+	//上下文
+		private static Context context = null;
+		//数据源
+		private static DataSource ds = null;
+		static{
+			try {
+				context = new InitialContext();
+				getDataSource();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("......关闭异常......");
 		}
-	}
-	public static void main(String[] args) {
-		System.out.println(DbHelp.getConnection());
-	}
+		public static DataSource getDataSource(){
+			try {
+				ds = (DataSource)context.lookup("java:comp/env/mysql");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return ds;
+		}
+		public static Connection getConnection(){
+			Connection conn = null;
+			try {
+				if(ds!=null){
+					conn = ds.getConnection();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return conn;
+		}
+		
+		public static void closeConnection(Connection conn){
+			try {
+				if(conn!=null && !conn.isClosed()){
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 }
