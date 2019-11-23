e62af7256a8ff7916159833329772cf1a34094f5
谭显 <77954@LAPTOP-CO8US9BN>
2019-11-23 21:27:21 +0800

    修改了UserDaoImpl,pojo.Acount, resource.chao.dao.impl.AcountDaoImpl

--- jyhwebstore/src/dao/impl/UserDaoImpl.java
+++ jyhwebstore/src/dao/impl/UserDaoImpl.java
@@ -1,8 +1,27 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.util.List;
+import java.util.UUID;
+
+import pojo.Acount;
+import chao.dao.AcountDao;
+import chao.dao.impl.AcountDaoImpl;
+import db.DbHelp;
+import db.DbHelp2;
+
 public class UserDaoImpl {
 private void mian() {
 	// TODO Auto-generated method stub
 
+}
+public static void main(String[] args) {
+	Connection conn= DbHelp2.getConnection();
+	
+AcountDao dao=new AcountDaoImpl();
+Acount acount=new Acount();
+acount.setAid("1");
+acount.setIsadm(1);
+dao.modifyAcount(acount, conn);
 }
 }
