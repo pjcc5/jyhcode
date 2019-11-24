b2c17ae8ed81b41cc69645b38c6c57b38e8b7787
pjcc5 <1783484759@qq.com>
2019-11-24 14:46:58 +0800

    写了详情表的dao和daoimple

--- jyhwebstore/resource/chao/dao/DetailsDao.java
+++ jyhwebstore/resource/chao/dao/DetailsDao.java
@@ -7,11 +7,11 @@ import pojo.Details;
 
 public interface DetailsDao {
 
-	public List<Details> getAllDetails();//获得所有的
-	public Details getDetailsById(String detailsid,Connection conn);//通过detailsid获得一个账户
-	public boolean insertDetails(Details details,Connection conn);//插入账户号的方法
-	public boolean deleteDetailsByid(String aid,Connection conn);//删除账号的方法
-	public boolean modifyDetails(Details details,Connection conn);//修改账号的方法
+	public List<Details> getAllDetails();//获得所有的详情
+	public Details getDetailsById(String detailsid,Connection conn);//通过detailsid获得一条详情
+	public boolean insertDetails(Details details,Connection conn);//插入详情的方法
+	public boolean deleteDetailsByid(String aid,Connection conn);//删除一条详情的方法
+	public boolean modifyDetails(Details details,Connection conn);//修改详情的方法
 	
 	
 }
---
+++ jyhwebstore/resource/chao/dao/impl/DetailsDaoImpl.java
@@ -0,0 +1,82 @@
+package chao.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import pojo.Details;
+import chao.dao.DetailsDao;
+
+public class DetailsDaoImpl implements DetailsDao{
+
+	@Override
+	public List<Details> getAllDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Details getDetailsById(String detailsid, Connection conn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertDetails(Details details, Connection conn) {
+		if(conn == null)
+		{
+			return false;
+		}
+		
+		try {
+			if(!conn.isClosed()){
+				String sql = "insert into details values(?,?,?,?,?,?,?,?,?,?,?,?)";
+				PreparedStatement ps=conn.prepareStatement(sql);
+				ps.setString(1, details.getDetailsid());
+				ps.setInt(2, details.getDetailsstock());
+				ps.setDouble(3, details.getDetailsprice());
+				ps.setString(4, details.getDetailsdrawing());
+				ps.setInt(5, details.getDetailsdot());
+				ps.setInt(6, details.getDetailssale());
+				ps.setString(7, details.getTest1());
+				ps.setString(8, details.getTest2());
+				ps.setString(9, details.getTest3());
+				ps.setString(10, details.getTest4());
+				ps.setString(11, details.getTest5());
+				ps.setString(12, details.getTest6());
+			int rs=ps.executeUpdate();
+			if(rs>0){
+				return true;
+			}
+			 
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return false;
+	}
+
+	@Override
+	public boolean deleteDetailsByid(String aid, Connection conn) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifyDetails(Details details, Connection conn) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
