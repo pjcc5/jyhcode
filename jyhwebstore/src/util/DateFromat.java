dcf4db326e57efd300b85d80f39e52a7c690153d
谭显 <77954@LAPTOP-CO8US9BN>
2019-11-25 22:32:14 +0800

    字符串转DATE

--- jyhwebstore/src/util/DateFromat.java
+++ jyhwebstore/src/util/DateFromat.java
@@ -1,16 +1,25 @@
 package util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateFromat {
-	public static String DateFormat(Date date){
+	public static String DateFormatToString(Date date){
 		if(date==null){
 			return null;
 		}
 		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String time=format.format(date);
 		return time;
+	}
+	public static Date StringFormattoDate(String date) throws ParseException{
+		if(date==null){
+			return null;
+		}
+		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date d=format.parse(date);
+		return d;
 	}
 
 }
