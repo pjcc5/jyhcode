93f81f65e419d58f68e5255cc8e71277658e1e6c
谭显 <77954@LAPTOP-CO8US9BN>
2019-11-25 16:39:02 +0800

    1234

--- jyhwebstore/resource/chao/dao/OrderDao.java
+++ jyhwebstore/resource/chao/dao/OrderDao.java
@@ -8,9 +8,9 @@ import pojo.Order;
 public interface OrderDao {
 	
 	public List<Order> getAllOrder(Connection conn);//获得所有的订单项
-	public Order getOrderById(String aid,Connection conn);//通过aid获得一个订单项
+	public Order getOrderById(int aid,Connection conn);//通过aid获得一个订单项
 	public boolean insertOrder(Order order,Connection conn);//插入订单项的方法
-	public boolean deleteOrderByid(String aid,Connection conn);//删除订单项的方法
+	public boolean deleteOrderByid(int aid,Connection conn);//删除订单项的方法
 	public boolean modifyOrder(Order order,Connection conn);//修改订单项的方法
 	
 
