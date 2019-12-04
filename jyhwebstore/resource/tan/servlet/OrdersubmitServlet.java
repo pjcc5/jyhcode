485a879e87d93cb1c16253299c8ed42d45f25a26
pjcc5 <1783484759@qq.com>
2019-12-04 13:29:01 +0800

    订单页面查找完成(bug)

--- jyhwebstore/resource/tan/servlet/OrdersubmitServlet.java
+++ jyhwebstore/resource/tan/servlet/OrdersubmitServlet.java
@@ -25,32 +25,12 @@ import db.DbHelp2;
 
 public class OrdersubmitServlet extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		this.doPost(request, response);
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
@@ -105,10 +85,6 @@ public class OrdersubmitServlet extends HttpServlet {
 		 }
 		 dao1.UpdateCart(list, conn, aid);
 		}
-		
-		
-		
-		
 
 	}
 
